7f932124d50e6f2e8276e9acd8493da5fc78ee9d
Louis Martin <louimart@gmail.com>
2024-02-13 21:26:16 -0600

    initial commit. GET saga function working

--- src/components/PlantForm/PlantForm.jsx
+++ src/components/PlantForm/PlantForm.jsx
@@ -5,7 +5,7 @@ const PlantForm = () => {
     const dispatch = useDispatch();
     
     //Initial state is an OBJECT, with keys id and name
-    let [newPlant, setPlant] = useState({id: 4, name: ''});
+    let [newPlant, setPlant] = useState({name: '', });
 
     const handleNameChange = (event) => {
         //Similar to in redux -- we dont want to get rid of the id field when we update name
@@ -14,16 +14,24 @@ const PlantForm = () => {
 
     const addNewPlant = event => {
         event.preventDefault();
-        dispatch({ type: 'ADD_PLANT', payload: newPlant });
+        // dispatch({ type: 'ADD_PLANT', payload: newPlant });
         //updates the next plant to have a new id
-        setPlant({id:newPlant.id + 1, name: ''});
+        // setPlant({id:newPlant.id + 1, name: ''})
+        
+        dispatch({ type: 'POST_PLANTS', payload: newPlant});
     }
     return (
         <div>
             <h3>This is the form</h3>
             <pre>{JSON.stringify(newPlant)}</pre>
             <form onSubmit={addNewPlant}>
-                <input type='text' value={newPlant.name} onChange={handleNameChange} />
+                <input type='text' value={newPlant.name} placeholder='name' onChange={handleNameChange} />
+                <input type='text' value={newPlant.kingdom} placeholder='kingdom' onChange={handleNameChange} />
+                <input type='text' value={newPlant.clade} placeholder='clade' onChange={handleNameChange} />
+                <input type='text' value={newPlant.order} placeholder='order' onChange={handleNameChange} />
+                <input type='text' value={newPlant.family} placeholder='family' onChange={handleNameChange} />
+                <input type='text' value={newPlant.subfamily} placeholder='subfamily' onChange={handleNameChange} />
+                <input type='text' value={newPlant.genus} placeholder='genus' onChange={handleNameChange} />
                 <input type='submit' value='Add New Plant' />
             </form>
         </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,8 @@
-import { createStore, combineReducers } from 'redux';
+import createSagaMiddleware from '@redux-saga/core';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
 
 // this startingPlantArray should eventually be removed
 const startingPlantArray = [
@@ -7,14 +11,76 @@ const startingPlantArray = [
   { id: 3, name: 'Oak' }
 ];
 
-const plantList = (state = startingPlantArray, action) => {
+const plantList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PLANT':
       return [ ...state, action.payload ]
+    case 'SET_PLANTS':
+      return action.payload
     default:
       return state;
   }
 };
+
+// SAGA
+const sagaMiddleware = createSagaMiddleware();
+
+// SAGA Generator Functions
+function* getPlantsSaga(action) {
+  console.log('running First Saga', action);
+  // try catch block
+  try {
+  // code to try running HERE
+  // wait for async event
+  const plantsResponse = yield axios({
+    method: 'GET',
+    url: '/api/plants',
+  });
+// dispatch to SET_ELEMENTS with elementsResponse.data
+// async event to dispatch as `put`
+yield put({ type: 'SET_PLANTS', payload: plantsResponse.data });
+} catch (error) {
+console.log('ERROR:', error);
+yield put({
+type: 'ERROR_PLANTS',
+payload: 'Something went wrong, please try again later.',
+});
+}
+}
+
+
+// SAGA POST Plants
+function* postPlantsSaga(action) {
+  try {
+    yield axios ({
+      method: 'POST',
+      url: '/api/plants',
+      data: {
+        name: action.payload.name,
+        kingdom: action.payload.kingdom,
+        clade: action.payload.clade,
+        family: action.payload.family,
+        subfamily: action.payload.subfamily,
+        genus: action.payload.genus,
+      }
+    });
+    yield put({ type: 'GET_PLANTS'});
+  } catch (error) {
+    console.log('ERROR: ', error);
+    yield put ({
+      type: 'ERROR_PLANTS',
+      payload: 'could not add plant at this time.'
+    });
+  }
+}
+
+
+// SAGA function [generator function]
+function* watcherSaga() {
+  // yield
+  yield takeEvery('GET_PLANTS', getPlantsSaga);
+  yield takeEvery('POST_PLANTS', postPlantsSaga);
+}
 
 // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 // Note that the store is currently not
@@ -22,7 +88,11 @@ const plantList = (state = startingPlantArray, action) => {
 // redux logger!
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware( sagaMiddleware, logger )
 );
+
+sagaMiddleware.run(watcherSaga);
+
 // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 
 export default store;
